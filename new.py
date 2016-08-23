b72dedb33ac38f2dfa5d8c8faa1e21a9e4c61013
rjbetz298 <rjbetz298@gmail.com>
2016-08-22 21:12:17 -0400

    Looping for Elephants List

--- new.py
+++ new.py
@@ -1,10 +1,19 @@
-
 import json
 
-json_data = open("AllCards.json","r")
-#print type(json_data) type lets you see if it is a file or string
+json_data = open("C:\Users\RyanRex\Desktop\AllCards.json", "r")
+# print type(json_data) type lets you see if it is a file or string
 data = json_data.read()
 
 card_dict = json.loads(data)
 
-#lets search through the dict for something?!?
+# lets search through the dict for something?!?
+
+Elephant_List = []
+for card_name,card in card_dict.iteritems():
+    try:
+        for subtype in card["subtypes"]:
+            if subtype == "Elephant":
+                Elephant_List.append([card_name])
+    except KeyError:
+        continue #if in a loop it goes to the next item
+print Elephant_List
